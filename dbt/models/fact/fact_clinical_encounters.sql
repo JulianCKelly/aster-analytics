e7094b7922b1a9e0c791506c62e6a02677491ec0
Julian Kelly <juliancharlankelly@gmail.com>
2026-06-28 15:06:27 -0700

    Fix truncated JOIN condition in fact_clinical_encounters

--- dbt/models/fact/fact_clinical_encounters.sql
+++ dbt/models/fact/fact_clinical_encounters.sql
@@ -16,4 +16,4 @@ SELECT
     e.source_format,
     CASE WHEN e.country != p.country_of_origin THEN TRUE ELSE FALSE END AS is_cross_border
 FROM {{ ref('stg_clinical_encounters') }} e
-LEFT JOIN {{ ref('stg_patients') }} p ON e.patient_id =
+LEFT JOIN {{ ref('stg_patients') }} p ON e.patient_id = p.patient_id
